79e2870fbb895a46003ad9c0750d16224d77fe61
Draga Doncila Pop <ddon0001@student.monash.edu>
2023-08-24 17:12:42 +1000

    Update widget to use Container subclass, add focus checkbox

--- src/tracktool/_napari/_plugin.py
+++ src/tracktool/_napari/_plugin.py
@@ -1,8 +1,9 @@
 from enum import Enum, auto
+from typing import Any, Sequence
 import warnings
 from magicgui import magic_factory
+from magicgui.widgets import Container, ComboBox, create_widget
 import numpy as np
-from skimage.measure import points_in_poly
 import networkx as nx
 
 # class MERGE_SET(Enum):
@@ -30,85 +31,128 @@ class MockEvent:
     def __init__(self, position):
         self.position = position
 
-def connect_merge_node_updates(merge_selection_widget):
-    merge_selection_widget.graph_layer.changed.connect(
-        lambda _: _update_merge_nodes(merge_selection_widget)
-    )
-    _update_merge_nodes(merge_selection_widget)
+class MergeExplorer(Container):
+    def __init__(
+        self,
+        viewer: 'napari.viewer.Viewer',
+        layout: str = "vertical",
+        labels: bool = True,
+    ) -> None:
+        super().__init__(
+            layout=layout,
+            labels=labels,
+        )
+        self._viewer = viewer
+        self._lineage_dc = None
+        
+        self._graph_layer_combo = create_widget(annotation="napari.layers.Graph", label='Graph Layer')
+        self._merge_id_combo = ComboBox(label="Merged Cells")
+        self._focus_mode_check = create_widget(annotation=bool, label="Focus Mode")
+        
+        self._graph_layer_combo.changed.connect(self._update_merge_node_choices)
+        self._graph_layer_combo.changed.connect(self._update_lineage_callback)
+        self._merge_id_combo.changed.connect(self._show_selected_merge)
+        self._focus_mode_check.changed.connect(self._handle_focus_mode_change)
+        
+        dw, self._arboretum = viewer.window.add_plugin_dock_widget("napari-arboretum", "Arboretum")
+        
+        self.extend([self._graph_layer_combo, self._merge_id_combo, self._focus_mode_check])    
+        self._update_merge_node_choices()
+        self._update_lineage_callback()
+        
+    def _update_merge_node_choices(self):
+        chosen_layer = self._graph_layer_combo.value
+        if "nxg" in chosen_layer.metadata:
+            nxg = chosen_layer.metadata["nxg"]
+        else:
+            nxg = chosen_layer.data.to_networkx()
+            chosen_layer.metadata["nxg"] = nxg
 
-def _update_merge_nodes(merge_selection_widget):
-    chosen_layer = merge_selection_widget.graph_layer.value
-    if 'nxg' in chosen_layer.metadata:
-        nxg = chosen_layer.metadata['nxg']
-    else:
-        nxg = chosen_layer.data.to_networkx()
-        chosen_layer.metadata['nxg'] = nxg
+        def get_choices(_merge_id_combo):
+            merge_nodes = [node for node in nxg.nodes if nxg.in_degree(node) > 1]
+            if not len(merge_nodes):
+                warnings.warn(
+                    f"Graph {chosen_layer.name} contains no merges! Try a different layer."
+                )
+            return merge_nodes
+
+        self._merge_id_combo.choices = get_choices
     
-    def get_choices(merge_node_combo):
-        merge_nodes = [node for node in nxg.nodes if nxg.in_degree(node) > 1]
-        if not len(merge_nodes):
-            warnings.warn(f'Graph {chosen_layer.name} contains no merges! Try a different layer.')
-        return merge_nodes
+    def _update_lineage_callback(self):
+        if self._lineage_dc is not None:
+            self._lineage_dc()
+        chosen_layer = self._graph_layer_combo.value
+        self._lineage_dc = chosen_layer.mouse_double_click_callbacks.append(self._show_lineage_tree)
+        
+    def _show_lineage_tree(self, layer, event):
+        if self._arboretum is None:
+            return
+        tracks_layer = layer.metadata["tracks"]
+        position = event.position
+        ev = MockEvent(position)
+        self._arboretum.show_tree(tracks_layer, ev)
+        
+        # if focus mode is on
+        if self._focus_mode_check.value:
+            self.color_nodes_in_tree(layer)
+            
+    def _show_selected_merge(self):
+        merge_id = self._merge_id_combo.value
+        graph_layer = self._graph_layer_combo.value
+        
+        if merge_id is None:
+            return
+        # make this the only visible graph layer
+        for layer in self._viewer.layers:
+            if layer._type_string == "graph":
+                layer.visible = False
+        graph_layer.visible = True
 
-    merge_selection_widget.merge_id.choices = get_choices
+        # get nxg from graph_layer
+        nxg = graph_layer.metadata["nxg"]
+        node_info = nxg.nodes[merge_id]
 
+        # center camera
+        self._viewer.dims.current_step = (node_info["t"], 0, 0)
+        self._viewer.camera.center = (node_info["y"], node_info["x"])
+        self._viewer.camera.zoom = 6
+        
+        # show lineage tree
+        event = MockEvent((node_info["t"], node_info["y"], node_info["x"]))
+        self._show_lineage_tree(graph_layer, event)
+        self._viewer.layers.selection.select_only(graph_layer)
+        
+    def _handle_focus_mode_change(self):
+        chosen_layer = self._graph_layer_combo.value
+        if self._focus_mode_check.value:
+            self.color_nodes_in_tree(chosen_layer)
+        else:
+            if "old-color" in chosen_layer.metadata:
+                nxg = chosen_layer.metadata['nxg']
+                nx.set_node_attributes(nxg, chosen_layer.metadata["old-color"], "color")            
+                chosen_layer.face_color = list(nx.get_node_attributes(nxg, "color").values())
 
-@magic_factory(
-        widget_init=connect_merge_node_updates,
-        merge_id = {"widget_type": "ComboBox"},
-        auto_call = True
-)
-def filter_merge_sets(graph_layer: 'napari.layers.Graph', merge_id, viewer:'napari.Viewer'):
-    if merge_id is None:
-        return
-    # make this the only visible graph layer
-    for layer in viewer.layers:
-        if layer._type_string == 'graph':
-           layer.visible = False
-    graph_layer.visible = True
-     
-    # get nxg from graph_layer
-    nxg = graph_layer.metadata['nxg']
-    node_info = nxg.nodes[merge_id]
-    
-    # center camera
-    viewer.dims.current_step = (node_info['t'], 0, 0)
-    viewer.camera.center = (node_info['y'], node_info['x'])
-    viewer.camera.zoom = 6
-    
-    # add dock widget (or just retrieve) and show tree for selected merge
-    tracks_layer = graph_layer.metadata['tracks']
-    position = (node_info['t'], node_info['y'], node_info['x'])
-    dw, widget = viewer.window.add_plugin_dock_widget("napari-arboretum", "Arboretum")
-    ev = MockEvent(position)
-    widget.show_tree(tracks_layer, ev)
-    viewer.layers.selection.select_only(graph_layer)
+    def color_nodes_in_tree(self, layer=None, ev=None):
+        nxg = layer.metadata['nxg']
+        if self._arboretum.plotter.has_nodes:
+            # restore face color
+            if "old-color" in layer.metadata:
+                nx.set_node_attributes(nxg, layer.metadata["old-color"], "color")
+            # store restored color
+            layer.metadata["old-color"] = nx.get_node_attributes(nxg, "color")
+            nx.set_node_attributes(nxg, DIMGREY, "color")
 
-    
-    # if this is the first time we've launched the widget, also add the double-click callback
-    if 'show_cb' not in graph_layer.metadata:
-        def show_tree_on_double_click(layer, event):
-            tracks_layer = layer.metadata['tracks']
-            ev = MockEvent(event.position)
-            widget.show_tree(tracks_layer, ev)
-            viewer.layers.selection.select_only(graph_layer)
-        dc = graph_layer.mouse_double_click_callbacks.append(show_tree_on_double_click)
-        graph_layer.metadata['show_cb'] = dc
-    
+            shown_tids = set([node.ID for node in self._arboretum.plotter._current_nodes])
+            tid_node_color = {}
+            # recolor nodes and set face color
+            for tid in shown_tids:
+                tid_node_color.update(
+                    {
+                        node: layer.metadata["old-color"][node]
+                        for node in nxg.nodes
+                        if nxg.nodes[node]["track-id"] == tid
+                    }
+                )
+            nx.set_node_attributes(nxg, tid_node_color, "color")
+            layer.face_color = list(nx.get_node_attributes(nxg, "color").values())
 
-    if widget.plotter.has_nodes and 'lineage_bbox' in viewer.layers:
-        # restore face color (where are we keeping it?)
-        if 'old-color' in graph_layer.metadata:
-            nx.set_node_attributes(nxg, graph_layer.metadata['old-color'], 'color')
-        # store restored color
-        graph_layer.metadata['old-color'] = nx.get_node_attributes(nxg, 'color')
-        nx.set_node_attributes(nxg, DIMGREY, 'color')
-
-        shown_tids = set([node.ID for node in widget.plotter._current_nodes])
-        tid_node_color = {}
-        # recolor nodes and set face color
-        for tid in shown_tids:
-            tid_node_color.update({node: graph_layer.metadata['old-color'][node] for node in nxg.nodes if nxg.nodes[node]['track-id'] == tid})
-        nx.set_node_attributes(nxg, tid_node_color, 'color')
-        graph_layer.face_color = list(nx.get_node_attributes(nxg, "color").values())
-    # how/when do we get the original colors restored?
