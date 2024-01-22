5081484913abd4dd11a7d87abe21f4858efc209c
Draga Doncila <ddon0001@student.monash.edu>
2024-01-22 15:10:51 +1100

    Start adding edges and edge costs

--- src/tracktour/_costs.py
+++ src/tracktour/_costs.py
@@ -29,33 +29,67 @@ def min_pairwise_distance_cost(child_distances):
     return min_dist
 
 
-def closest_neighbour_child_cost(parent_coords, child_coords):
-    min_dist = math.inf
-    for i, j in combinations(range(len(child_coords)), 2):
-        coords_i = child_coords[i]
-        coords_j = child_coords[j]
-        inter_child_dist = np.linalg.norm(coords_j - coords_i)
-        dist_i = np.linalg.norm(parent_coords - coords_i)
-        dist_j = np.linalg.norm(parent_coords - coords_j)
-        total_dist = inter_child_dist + min(dist_i, dist_j)
-        if total_dist < min_dist:
-            min_dist = total_dist
-    return min_dist
+# def closest_neighbour_child_cost(parent_coords, child_coords):
+#     min_dist = math.inf
+#     for i, j in combinations(range(len(child_coords)), 2):
+#         coords_i = child_coords[i]
+#         coords_j = child_coords[j]
+#         inter_child_dist = np.linalg.norm(coords_j - coords_i)
+#         dist_i = np.linalg.norm(parent_coords - coords_i)
+#         dist_j = np.linalg.norm(parent_coords - coords_j)
+#         total_dist = inter_child_dist + min(dist_i, dist_j)
+#         if total_dist < min_dist:
+#             min_dist = total_dist
+#     return min_dist
 
 
-def dist_to_edge_cost_func(bounding_box_dimensions, node_coords):
-    min_to_edge = math.inf
-    box_mins = [dim[0] for dim in bounding_box_dimensions]
-    # skip time coords as not relevant
-    for i in range(len(node_coords)):
-        box_dim_max = bounding_box_dimensions[1][i]
-        box_dim_min = bounding_box_dimensions[0][i]
+def closest_neighbour_child_cost(detections, location_keys, edge_df):
+    min_dists = []
+    for det_row in detections.itertuples():
+        potential_children = edge_df[edge_df["u"] == det_row.Index]
+        src_coords = np.asarray([getattr(det_row, key) for key in location_keys])
+        child_coords = {
+            v: np.asarray(detections.loc[v, list(location_keys)])
+            for v in potential_children["v"]
+        }
+        # let's also do closest child in here
+        interchild_distances = [
+            # distance between potential children in the next frame
+            np.linalg.norm(child_coords[u] - child_coords[v]) +
+            # distance from parent to closest potential child
+            min(
+                np.linalg.norm(src_coords - child_coords[u]),
+                np.linalg.norm(src_coords - child_coords[v]),
+            )
+            for u, v in combinations(child_coords.keys(), 2)
+        ]
+        min_dists.append(min(interchild_distances))
+        return min_dists
 
-        node_val = node_coords[i]
-        distance_to_min = node_val - box_dim_min
-        distance_to_max = box_dim_max - node_val
-        smallest = (
-            distance_to_min if distance_to_min < distance_to_max else distance_to_max
-        )
-        min_to_edge = min_to_edge if min_to_edge < smallest else smallest
-    return min_to_edge
+
+# def dist_to_edge_cost_func(bounding_box_dimensions, node_coords):
+#     min_to_edge = math.inf
+#     box_mins = [dim[0] for dim in bounding_box_dimensions]
+#     # skip time coords as not relevant
+#     for i in range(len(node_coords)):
+#         box_dim_max = bounding_box_dimensions[1][i]
+#         box_dim_min = bounding_box_dimensions[0][i]
+
+#         node_val = node_coords[i]
+#         distance_to_min = node_val - box_dim_min
+#         distance_to_max = box_dim_max - node_val
+#         smallest = (
+#             distance_to_min if distance_to_min < distance_to_max else distance_to_max
+#         )
+#         min_to_edge = min_to_edge if min_to_edge < smallest else smallest
+#     return min_to_edge
+
+
+# TODO: Do we want to support negative coords
+def dist_to_edge_cost_func(im_shape, detections, location_keys):
+    # distance from 0 is just the coordinate itself
+    dist_to_top_left = detections[list(location_keys)].values
+    dist_to_bottom_right = np.asarray(im_shape) - dist_to_top_left
+    dist_to_borders = np.concatenate((dist_to_top_left, dist_to_bottom_right), axis=1)
+    min_dists = np.min(dist_to_borders, axis=1)
+    return min_dists
--- src/tracktour/_tracker.py
+++ src/tracktour/_tracker.py
@@ -1,32 +1,53 @@
-import pandas as pd
-
-from scipy.spatial import KDTree
+from collections import defaultdict
 from typing import Optional, Tuple
 
-from ._costs import euclidean_cost_func, dist_to_edge_cost_func, closest_neighbour_child_cost
+import numpy as np
+import pandas as pd
+from scipy.spatial import KDTree
+from tqdm import tqdm
+
+from ._costs import (
+    closest_neighbour_child_cost,
+    dist_to_edge_cost_func,
+    euclidean_cost_func,
+)
+
 
 class Tracker:
+    def __init__(self, im_shape: Tuple[int, int], k_neighbours: int = 10) -> None:
+        self.im_shape = im_shape
+        self.k_neighbours = k_neighbours
 
-    def __init__(self, k_neighbours=10) -> None:
-        self._k_neighbours = k_neighbours
-
-        # if we want to make these public, we can add kwargs to init
-        self._enter_exit_cost = dist_to_edge_cost_func
-        self._migration_cost = euclidean_cost_func
-        self._div_cost = closest_neighbour_child_cost
-
-
-    def solve(self, detections: pd.DataFrame, time_key: str ='t', location_keys: Tuple[str]=('y', 'x'), value_key: Optional[str]=None):
+    def solve(
+        self,
+        detections: pd.DataFrame,
+        time_key: str = "t",
+        location_keys: Tuple[str] = ("y", "x"),
+        value_key: Optional[str] = None,
+    ):
         # build kd-trees
         kd_dict = self._build_trees(detections, time_key, location_keys)
 
-        # compute costs and store in node and edge dataframes
-        edge_df = self._compute_costs(detections, time_key, location_keys, kd_dict))
+        # get candidate edges
+        edge_df = self._get_candidate_edges(detections, time_key, kd_dict)
+
+        # migration cost (on edges) is just
+        edge_df["migration_cost"] = edge_df["distance"]
+
+        # compute costs for division and appearance/exit - on nodes
+        detections["enter_exit_cost"] = dist_to_edge_cost_func(
+            self.im_shape, detections, location_keys
+        )
+        detections["div_cost"] = closest_neighbour_child_cost(
+            detections, location_keys, edge_df
+        )
         # build model
 
-        # solve
+        # solve, edge_df
 
-    def _build_trees(self, detections: pd.DataFrame, time_key: str, location_keys: Tuple[str]):
+    def _build_trees(
+        self, detections: pd.DataFrame, time_key: str, location_keys: Tuple[str]
+    ):
         """Build dictionary of KDTrees for each frame in detections
 
         Parameters
@@ -44,12 +65,54 @@ class Tracker:
             dictionary of KDTree objects for each frame in detections
         """
         kd_dict = {}
-        sorted_ts = detections[time_key].sort_values()
+        sorted_ts = sorted(detections[time_key].unique())
         for t in sorted_ts:
-            frame_detections = detections[detections[time_key] == t][list(location_keys)]
+            frame_detections = detections[detections[time_key] == t][
+                list(location_keys)
+            ]
             kd_dict[t] = KDTree(frame_detections)
 
         return kd_dict
 
-    def _compute_costs(self, detections: pd.DataFrame, time_key: str, location_keys: Tuple[str], kd_dict):
-        pass
+    def _get_candidate_edges(self, detections: pd.DataFrame, time_key: str, kd_dict):
+        all_edges = defaultdict(list)
+        sorted_ts = sorted(detections[time_key].unique())
+        for source_t in tqdm(
+            sorted_ts[:-1], total=len(sorted_ts) - 1, desc="Computing candidate edges"
+        ):
+            dest_t = source_t + 1
+            source_frame_tree = kd_dict[source_t]
+            dest_frame_tree = kd_dict[dest_t]
+            if source_frame_tree.n == 0:
+                raise ValueError(f"KDTree for frame {source_t} is empty.")
+            if dest_frame_tree.n == 0:
+                raise ValueError(f"KDTree for frame {dest_t} is empty.")
+            # not querying for more neighbours than are in the frame
+            k = (
+                self.k_neighbours
+                if dest_frame_tree.n > self.k_neighbours
+                else dest_frame_tree.n
+            )
+            # passing 1 makes query return an int so we pass a list containing 1 instead
+            if k == 1:
+                k = [1]
+            k_closest_distances, k_closest_indices = dest_frame_tree.query(
+                source_frame_tree.data, k=k
+            )
+            all_edges["u"].append(
+                np.repeat(detections[detections.t == source_t].index.values, k)
+            )
+            # grab the vertex indices of closest neighbours using position indexing into detections
+            # TODO: dangerous? maybe...
+            all_edges["v"].append(
+                detections[detections.t == dest_t]
+                .iloc[k_closest_indices.ravel()]
+                .index.values
+            )
+            all_edges["distance"].append(k_closest_distances.ravel())
+
+        all_edges["u"] = np.concatenate(all_edges["u"])
+        all_edges["v"] = np.concatenate(all_edges["v"])
+        all_edges["distance"] = np.concatenate(all_edges["distance"])
+        edge_df = pd.DataFrame(all_edges)
+        return edge_df
