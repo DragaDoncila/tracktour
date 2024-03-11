a5c7dc5a3da358d3e7dba27c9cde57afd73b2080
Draga Doncila <ddon0001@student.monash.edu>
2024-03-11 16:04:49 +1100

    Fix optional dep listing, add napari-graph to it

--- src/tracktour/_napari/__init__.py
+++ src/tracktour/_napari/__init__.py
@@ -2,12 +2,19 @@ try:
     import napari
 except ModuleNotFoundError as e:
     raise RuntimeError(
-        "napari not found. Cannot use plugin functionality without it. Did you install `tracktour[all]`?"
+        "napari not found. Cannot use plugin functionality without it. Did you install `tracktour[napari]`?"
+    ) from e
+
+try:
+    import napari_graph
+except ModuleNotFoundError as e:
+    raise RuntimeError(
+        "napari_graph not found. Cannot use plugin functionality without it. Did you install `tracktour[napari]`?"
     ) from e
 
 try:
     import napari_arboretum
 except ModuleNotFoundError as e:
     raise RuntimeError(
-        "napari_arboretum not found. Cannot use plugin functionality without it. Did you install `tracktour[all]`?"
+        "napari_arboretum not found. Cannot use plugin functionality without it. Did you install `tracktour[napari]`?"
     ) from e
